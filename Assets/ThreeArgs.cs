59eeed43bfd4f4a9e0eba53b10e3075ab1809c4c
Chris_H <cd.hardwick@gmail.com>
2021-11-17 21:18:01 +0000

    Change 3 Args to ThreeArgs

--- Assets/ThreeArgs.cs
+++ Assets/ThreeArgs.cs
@@ -2,25 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThreeArgs : MonoBehaviour
+public class ThreeArgs : MonoBehaviour //Q2d
 {
     public TMPro.TMP_Text text_display;
 
-    public string First = "Angela";
-    public string Second = "Maria";
-    public string Third = "Riley";
-
-    public string TheName;
+    public string First = " Angela ";
+    public string Second = " Maria ";
+    public string Third = " Riley ";
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        text_display.text = "Hello" + "," + First + "," + Second + "," + Third; //Q2d
     }
 
     // Update is called once per frame
     void Update()
     {
-        text_display.text = TheName;
+        text_display.text = "Hello" + "," + First + "," + Second + "," + Third; //Q2e
     }
 }
