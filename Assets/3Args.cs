19be057a479e5cd6dbe36e96818b0e8de27c99c3
Chris_H <cd.hardwick@gmail.com>
2021-11-17 21:02:03 +0000

    BackUp

--- Assets/3Args.cs
+++ Assets/3Args.cs
@@ -2,20 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloTextMeshPro : MonoBehaviour //Q2a
+public class 3_Args: MonoBehaviour
 {
-    public TMPro.TMP_Text text_display;
-    public string TheName = "Type Name Here";
-
     // Start is called before the first frame update
     void Start()
     {
-        text_display.text = TheName;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        text_display.text = TheName; //Q2c
+        
     }
 }
--- Assets/ThreeArgs.cs
+++ Assets/ThreeArgs.cs
@@ -2,20 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloTextMeshPro : MonoBehaviour //Q2a
+public class ThreeArgs : MonoBehaviour
 {
     public TMPro.TMP_Text text_display;
-    public string TheName = "Type Name Here";
+
+    public string First = "Angela";
+    public string Second = "Maria";
+    public string Third = "Riley";
+
+    public string TheName;
 
     // Start is called before the first frame update
     void Start()
     {
-        text_display.text = TheName;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        text_display.text = TheName; //Q2c
+        text_display.text = TheName;
     }
 }
