ed992d95a53d1c7111e041bb0012f32bb7843e57
Chris_H <cd.hardwick@gmail.com>
2021-11-20 22:55:31 +0000

    BackUp Commit

--- Assets/Scripts/Q4_IfStatements01.cs
+++ Assets/Scripts/Q4_IfStatements01.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Q4_IfStatements01 : MonoBehaviour
 {
+    public TMPro.TMP_Text Atext, Btext;
+
+    public TMPro.TMP_Text Q4a, Q4b, Q4c, Q4d, Q4e, Q4f;
+
+    public int A, B;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,7 @@ public class Q4_IfStatements01 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Atext.text = ("A = " + A.ToString());
+        Btext.text = ("B = " + B.ToString());
     }
 }
